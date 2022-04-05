fb5dcf4e7694d864744a1a8637b0635ead71cf26
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2022-04-05 10:57:42 -0300

    :tada: inicialização do projeto

---
+++ agamemnon-rh/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Routes, BrowserRouter, Route, Link } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>Teste</div>
+    );
+  }
+}
+
+export default App;
