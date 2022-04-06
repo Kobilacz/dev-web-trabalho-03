4c9292ce5a12fd4401c76370f163d01da877b970
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2022-04-06 11:37:06 -0300

    :sparkles: criação de App.css e App.js

--- agamemnon-rh/src/App.js
+++ agamemnon-rh/src/App.js
@@ -3,10 +3,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Routes, BrowserRouter, Route, Link } from "react-router-dom";
 
+import AddFuncionario from "./components/addFuncionario";
+import ListFuncionario from "./components/listFuncionario";
+
 class App extends Component {
   render() {
     return (
-      <div>Teste</div>
+      <div>
+        <BrowserRouter>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <div className="container">
+              <Link to={"/list"} className="navbar-brand">
+                <b>
+                  <i>Agamemnon RH</i>
+                </b>
+              </Link>
+              <div className="navbar-nav mr-auto">
+                <li className="nav_item">
+                  <Link to={"/add"} className="nav-link">
+                    Adicionar funcionário
+                  </Link>
+                </li>
+                <li className="nav_item">
+                  <Link to={"/list"} className="nav-link">
+                    Listar todos os funcionários
+                  </Link>
+                </li>
+              </div>
+            </div>
+          </nav>
+          <div className="container mt-3">
+            <Routes>
+              <Route element={<ListFuncionario />} path="/" />
+              <Route element={<ListFuncionario />} path="/list" />
+              <Route element={<AddFuncionario />} path="/add" />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
