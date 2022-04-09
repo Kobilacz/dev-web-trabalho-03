4107465469f60b79aa3b9f2ad4aed247b6844d9b
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2022-04-09 00:20:40 -0300

    :lipstick: ajustes na página listFuncionario

--- agamemnon-rh/src/components/listFuncionario.js
+++ agamemnon-rh/src/components/listFuncionario.js
@@ -7,6 +7,7 @@ export default class ListFuncionario extends Component {
     super(props);
 
     this.retrieveFuncionarios = this.retrieveFuncionarios.bind(this);
+    this.removeAll = this.deleteAll.bind(this);
 
     this.state = {
       funcionarios: [],
@@ -30,17 +31,82 @@ export default class ListFuncionario extends Component {
       });
   }
 
+  deleteAll() {
+    FuncionarioDataService.deleteAll()
+      .then((response) => {
+        this.setState((prevState) => {
+          console.log(prevState);
+          return { funcionarios: [] };
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  deleteFuncionario(id) {
+    FuncionarioDataService.delete(id)
+      .then((response) => {
+        this.setState((prevState) => {
+          console.log(prevState);
+          return { funcionarios: prevState.funcionarios.filter((func) => func.id !== id) };
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   render() {
     const { funcionarios } = this.state;
 
     return (
-      <div className="list row">
-        <div className="col-md-6">
-          <h4>Funcionários</h4>
-          <ul className="list-group">
-            {funcionarios && funcionarios.map((funcionario) => <li>{funcionario.nome}</li>)}
-          </ul>
-        </div>
+      <div className="table-container">
+        {funcionarios.length > 0 ? (
+          <div>
+            <h3 className="title-container">
+              <b>Funcionários</b>
+            </h3>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Nome</th>
+                  <th scope="col">CPF</th>
+                  <th scope="col">Ação</th>
+                </tr>
+              </thead>
+              <tbody>
+                {funcionarios.map((funcionario) => (
+                  <tr key={funcionario.id}>
+                    <td>{funcionario.nome}</td>
+                    <td className="centralized">{funcionario.cpf}</td>
+                    <td className="centralized">
+                      {
+                        <button
+                          className="btn btn-primary"
+                          onClick={() => {
+                            this.deleteFuncionario(funcionario.id);
+                          }}
+                        >
+                          Excluir
+                        </button>
+                      }
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className="centralized-button">
+              <button className="btn btn-primary" onClick={this.removeAll}>
+                Excluir todos os funcionários
+              </button>
+            </div>
+          </div>
+        ) : (
+          <h3 className="centralized">Nenhum funcionário cadastrado!</h3>
+        )}
       </div>
     );
   }
