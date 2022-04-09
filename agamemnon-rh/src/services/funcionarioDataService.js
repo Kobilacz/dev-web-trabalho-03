fe8ba9cc98d80f9d852e22d93fb22416b9a5b6d9
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2022-04-09 00:20:00 -0300

    :sparkles: adição de exclusão total e por id

--- agamemnon-rh/src/services/funcionarioDataService.js
+++ agamemnon-rh/src/services/funcionarioDataService.js
@@ -8,6 +8,14 @@ class FuncionarioDataService {
   create(data) {
     return http.post("/funcionarios", data);
   }
+
+  delete(id) {
+    return http.delete(`/funcionarios/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete("/funcionarios");
+  }
 }
 
 export default new FuncionarioDataService();
