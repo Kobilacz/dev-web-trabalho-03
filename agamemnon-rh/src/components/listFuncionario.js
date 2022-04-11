f21d9c58a0f256545afe191eb1fa0d64c5e59aeb
eliezerdsv <eliezerdsv@gmail.com>
2022-04-10 22:02:06 -0300

    :lock: remocao de informacoes inutilizadas

--- agamemnon-rh/src/components/listFuncionario.js
+++ agamemnon-rh/src/components/listFuncionario.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import FuncionarioDataService from "../services/funcionarioDataService";
-import { Link } from "react-router-dom";
 
 export default class ListFuncionario extends Component {
   constructor(props) {
